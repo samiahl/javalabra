787695285cb64ed42f85e725dce985812e97ef4f
Sami Åhl <sami.leo.ahl@gmail.com>
2013-05-21 16:40:35 +0300

    lisätty laudan luonti Pelilauta-luokalle

--- OlutPeli/src/olutpeli/Pelilauta.java
+++ OlutPeli/src/olutpeli/Pelilauta.java
@@ -12,12 +12,21 @@ import java.util.List;
  * @author samiahl
  */
 public class Pelilauta {
+
     private List taulu;
+
+    public Pelilauta() {  // koko tulee olemaan joko 2x2, 4x4 tai 6x6
+        this.taulu = new ArrayList<>();
+    }
+
+    public void luoPelilauta(int koko) {
+        for (int i = 0; i < koko; i++) {
+            taulu.add(luoLista());
+        }
+    }
     
-    
-    public Pelilauta(int koko){
-        this.taulu = new ArrayList<ArrayList>();
-   
-        
+    public ArrayList luoLista() {
+        ArrayList<Kortti> rivi = new ArrayList<>();
+        return rivi;
     }
 }
