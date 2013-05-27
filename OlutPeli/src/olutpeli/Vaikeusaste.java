638ff5024d95ec27c62d7f1a4e958ff35272079b
Sami Åhl <sami.leo.ahl@gmail.com>
2013-05-27 17:29:01 +0300

    lisätty vaikeusaste-enumit, jota ehkä käytän tai sitten en...

---
+++ OlutPeli/src/olutpeli/Vaikeusaste.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olutpeli;
+
+/**
+ *
+ * @author samiahl
+ */
+public enum Vaikeusaste {
+    HELPPO, NORMAALI, VAIKEA
+}
